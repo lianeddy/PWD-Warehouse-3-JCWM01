d35bb1469d1f4a783b67e0eb1009b6c796d75d23
Sarewes <35771197+sarewes2310@users.noreply.github.com>
2021-09-29 16:09:49 +0700

    Tambah file json dan sql untuk data migrations

---
+++ database/sql/app_category_master_produk.sql
@@ -0,0 +1,8 @@
+SELECT * FROM purwadhika_project_akhir.app_category_master_produk;
+
+insert into app_category_master_produk (nm_category_master_produk, created_at, updated_at) 
+values
+('Fitness', now(), now()),
+('Medical', now(), now()),
+('Entertainment', now(), now()),
+('Lifestyle', now(), now());
---
+++ database/sql/app_master_produk.sql
@@ -0,0 +1,10 @@
+SELECT * FROM purwadhika_project_akhir.app_master_produk;
+
+insert into app_master_produk (nm_master_produk, harga, id_category, created_at, updated_at) 
+values 
+("Barska GB12166 Fitness Watch",49.99,1, now(), now()),
+("Belkin GS5 Sport Fit Armband",24.99,1, now(), now()),
+("Bowflex EZ Pro",12.99,2, now(), now()),
+('Casio G Shock',67.62,4, now(), now()),
+('Casio WS220 Solar',39.99,4, now(), now()),
+('Coleman G7HD-SWIM',167.99,3, now(), now());
