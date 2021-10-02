9b71d19db2cee76bc972139fdb19c7407c635a99
Sarewes <35771197+sarewes2310@users.noreply.github.com>
2021-10-02 19:31:52 +0700

    tambah relation mappings ke model AppKabKota

--- database/table/AppPropinsi.js
+++ database/table/AppPropinsi.js
@@ -1,8 +1,22 @@
+const AppKabKota = require("./AppKabKota");
 const { ProjectAkhirPurwadhika } = require("../mysql/ProjectAkhirPurwadhika");
 
 class AppPropinsi extends ProjectAkhirPurwadhika {
   static get tableName() {
     return "app_propinsi";
+  }
+
+  static get relationMappings() {
+    return {
+      datakabkota: {
+        relation: ProjectAkhirPurwadhika.HasManyRelation,
+        modelClass: AppKabKota,
+        join: {
+          from: "app_propinsi.id_propinsi",
+          to: "app_kabkota.id_propinsi",
+        },
+      },
+    };
   }
 }
 
