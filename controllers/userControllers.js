b9da54f6813c0abee49694153d4adadcec682c6c
armerayfr <armerray@gmail.com>
2021-10-05 20:38:41 +0700

    update status http & styling

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -11,7 +11,7 @@ module.exports = {
     )} and password = ${db.escape(req.body.password)}`;
     console.log(req.body);
     db.query(scriptQuery, (err, results) => {
-      console.log(results[0]);
+      console.log(results);
       if (err) res.status(500).send(err);
       if (results[0]) {
         let { id_user, id_warehouse, username, email, password, is_valid } =
@@ -25,13 +25,15 @@ module.exports = {
           password,
           is_valid,
         });
-        if ((is_valid = 0)) {
-          res.status(200).send({ message: "Your account not verified" });
+        if (is_valid === 0) {
+          res.status(404).send({ message: "Your account not verified" });
         } else {
           res
             .status(200)
             .send({ dataLogin: results[0], token, message: "Login Success" });
         }
+      } else {
+        res.status(400).send({ message: "email/password not match" });
       }
     });
   },
--- src/jsx/pages/login.js
+++ src/jsx/pages/login.js
@@ -10,7 +10,7 @@ class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      alertError: "none",
+      alertShow: "none",
       redirect: false,
     };
   }
@@ -20,71 +20,86 @@ class Login extends React.Component {
       email: this.inputEmail.value,
       password: this.inputPassword.value,
     })
-      .then((res) => {
-        localStorage.setItem("dataToken", res.data.token);
-        //action
-        this.props.authLogin(res.data.dataLogin);
-        this.setState({ redirect: true });
-        console.log("Login Success");
-        this.inputUsername.value = "";
-        this.inputPassword.value = "";
+      .then((result) => {
+        console.log(result.data.length);
+        if ((result.data.length = undefined)) {
+          this.setState({ alertShow: "block" });
+        } else {
+          localStorage.setItem("dataToken", result.data.token);
+          //action
+          this.props.authLogin(result.data.dataLogin);
+          this.setState({ redirect: true });
+          console.log("Login Success");
+          this.inputUsername.value = "";
+          this.inputPassword.value = "";
+        }
       })
       .catch((err) => console.log(err));
   };
 
   render() {
     if (this.state.redirect) {
+      // this.setState({})
       return <Redirect to="/" />;
     }
 
     return (
-      <form>
-        <h3>Sign In</h3>
-
-        <div className="form-group">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            ref={(e) => (this.inputEmail = e)}
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            ref={(e) => (this.inputPassword = e)}
-          />
-        </div>
-
-        <div className="form-group">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
-
-        <button
-          type="button"
-          className="btn btn-primary btn-block"
-          onClick={this.onBtnLogin}
+      <div className="form-inner">
+        <div
+          className="alert alert-danger"
+          style={{ display: this.state.alertShow }}
+          role="alert"
         >
-          Login
-        </button>
-        <p className="forgot-password text-right">
-          <a href="#">Forgot password?</a>
-        </p>
-      </form>
+          Account not found !
+        </div>
+        <form>
+          <h3>Sign In</h3>
+
+          <div className="form-group">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              ref={(e) => (this.inputEmail = e)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              ref={(e) => (this.inputPassword = e)}
+            />
+          </div>
+
+          <div className="form-group">
+            <div className="custom-control custom-checkbox">
+              <input
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
+              />
+              <label className="custom-control-label" htmlFor="customCheck1">
+                Remember me
+              </label>
+            </div>
+          </div>
+
+          <button
+            type="button"
+            className="btn btn-primary btn-block"
+            onClick={this.onBtnLogin}
+          >
+            Login
+          </button>
+          <p className="forgot-password text-right">
+            <a href="#">Forgot password?</a>
+          </p>
+        </form>
+      </div>
     );
   }
 }
