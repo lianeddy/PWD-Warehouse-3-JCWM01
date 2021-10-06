bb3700cd21985719ff1f2d7b6a53721e64021042
Sarewes <35771197+sarewes2310@users.noreply.github.com>
2021-10-06 15:46:34 +0700

    Tambah StorageHelper dan RajaOngkirHelper

--- helpers/index.js
+++ helpers/index.js
@@ -1,11 +1,15 @@
+const StorageHelper = require("./StorageHelper");
 const Faker = require("./Faker");
 const TimeModule = require("./TimeModule");
 const MysqlConnector = require("./MysqlConnector");
 const RandomHelper = require("./RandomHelper");
+const RajaOngkirHelper = require("./RajaOngkirHelper");
 
 module.exports = {
   MysqlConnector,
   TimeModule,
   RandomHelper,
   Faker,
+  StorageHelper,
+  RajaOngkirHelper,
 };
