f0e04b72b26d5863ce4155038da3d97c8261f0e5
Sarewes <35771197+sarewes2310@users.noreply.github.com>
2021-10-08 10:41:06 +0700

    tambah endpoint untuk manajemen data alamat user

--- routers/userRouters.js
+++ routers/userRouters.js
@@ -1,8 +1,26 @@
 const express = require("express");
-const { userControllers } = require("../controllers");
+const {
+  userControllers,
+  AppDataAlamatUserController,
+  AppPropinsiController,
+  AppKabkotaController,
+  // UserImageController,
+} = require("../controllers");
 
 const routers = express.Router();
 
 routers.post("/login", userControllers.getData);
+
+// Endpoint CRUD multi-address user
+routers.get("/multi-address", AppDataAlamatUserController.getData);
+routers.post("/multi-address", AppDataAlamatUserController.addData);
+routers.patch("/multi-address/:id", AppDataAlamatUserController.updateData);
+routers.delete("/multi-address/:id", AppDataAlamatUserController.deleteData);
+routers.get("/multi-address/filter-provinsi", AppPropinsiController.getData);
+routers.get("/multi-address/filter-kabkota", AppKabkotaController.getData);
+
+// Profile Image
+// routers.post("/profile-image", UserImageController.addData);
+// routers.patch("/profile-image/:id", UserImageController.updateData);
 
 module.exports = routers;
