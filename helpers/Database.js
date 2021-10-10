d647cf57e8aa2b4c1e71a759367364798d6a9639
armerayfr <armerray@gmail.com>
2021-10-10 09:43:55 +0700

    login, register, verification done!

--- helpers/Database.js
+++ helpers/Database.js
@@ -1,6 +1,13 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_DATABASE,
+  // port: process.env.DB_PORT,
+  // multipleStatements: true,
   host: "localhost",
   user: "root",
   password: "coco1506",
--- src/jsx/pages/register.js
+++ src/jsx/pages/register.js
@@ -2,15 +2,17 @@ import React from "react";
 import Axios from "axios";
 import { URL_API } from "../../helper";
 import { connect } from "react-redux";
-// import { authLogin } from "../../redux/actions/user";
-import "./login.css";
-import { Redirect } from "react-router";
+import "./auth.css";
+import { Redirect } from "react-router-dom";
+import Swal from "sweetalert2";
 
 class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       redirect: false,
+      alertShow: "none",
+      disableBtn: false,
     };
   }
 
@@ -19,7 +21,9 @@ class Register extends React.Component {
     let email = this.inputEmail.value;
     let password = this.inputPassword.value;
 
-    if (username == "" || email == "" || password == "") {
+    this.setState({ disableBtn: true });
+
+    if (username === "" || email === "" || password === "") {
       alert("Fill in all the form");
     } else {
       Axios.post(`${URL_API}/users/register`, {
@@ -29,9 +33,15 @@ class Register extends React.Component {
       })
         .then((res) => {
           console.log(res.data.message);
-          alert("Register success, check your email for verification");
+          Swal.fire("Register success, check your email for verification");
+          this.setState({
+            alertShow: "block",
+            redirect: true,
+          });
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          console.log(err);
+        });
     }
   };
 
@@ -43,79 +53,61 @@ class Register extends React.Component {
     return (
       <div className="intro">
         <div className="form-inner">
-          {/* <div
-          className="alert alert-danger"
-          style={{ display: this.state.alertShow }}
-          role="alert"
-        >
-          Register success, check your email for verification
-        </div> */}
-          <form>
-            <h3>Sign Up</h3>
+          <div
+            className="alert alert-danger"
+            style={{ display: this.state.alertShow }}
+            role="alert"
+          >
+            Register success
+          </div>
 
-            <div className="form-group">
-              <label>Username</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Username"
-                ref={(e) => (this.inputUsername = e)}
-              />
-            </div>
+          <h3>Sign Up</h3>
 
-            <div className="form-group">
-              <label>Email address</label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Enter email"
-                ref={(e) => (this.inputEmail = e)}
-              />
-            </div>
+          <div className="form-group">
+            <label>Username</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Username"
+              ref={(e) => (this.inputUsername = e)}
+            />
+          </div>
 
-            <div className="form-group">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Enter password"
-                ref={(e) => (this.inputPassword = e)}
-              />
-            </div>
+          <div className="form-group">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              ref={(e) => (this.inputEmail = e)}
+            />
+          </div>
 
-            <div className="form-group">
-              <div className="custom-control custom-checkbox">
-                <input
-                  type="checkbox"
-                  className="custom-control-input"
-                  id="customCheck1"
-                  onClick={this.togglePassword}
-                />
-                <label className="custom-control-label" htmlFor="customCheck1">
-                  Show Password
-                </label>
-              </div>
-            </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              ref={(e) => (this.inputPassword = e)}
+            />
+          </div>
 
-            <button
-              type="submit"
-              className="btn btn-primary btn-block"
-              onClick={this.onBtnRegister}
-            >
-              Sign Up
-            </button>
-            <p className="forgot-password text-right">
-              Already registered <a href="/login">sign in?</a>
-            </p>
-          </form>
+          <button
+            type="submit"
+            className="btn btn-primary btn-block btn-auth"
+            onClick={this.onBtnRegister}
+            disabled={this.state.disableBtn}
+          >
+            Sign Up
+          </button>
+          <p className="forgot-password text-right">
+            Already registered <a href="/login">sign in?</a>
+          </p>
         </div>
       </div>
     );
   }
 }
-
-// const mapDispatchtoProps = {
-//   authLogin,
-// };
 
 export default connect(null, null)(Register);
