df52e3482125421c0fb00eedc3e707700b250597
Sarewes <35771197+sarewes2310@users.noreply.github.com>
2021-10-13 12:13:56 +0700

    tambah props untuk menyambungkan button filter

--- src/jsx/components/BtnFilterCari.jsx
+++ src/jsx/components/BtnFilterCari.jsx
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 import { Col, Button, Row } from "react-bootstrap";
 import { Typeahead } from "react-bootstrap-typeahead";
 
-const BtnFilterCari = (props) => {
+const BtnFilterCari = ({ onReset, onCari }) => {
   return (
     <Row>
       <Col sm="12" md="3" lg="4" lg="4" />
       <Col sm="12" md="3" lg="6" lg="6">
-        <Button variant="secondary" className="me-2">
+        <Button variant="secondary" className="me-2" onClick={onReset}>
           Reset
         </Button>
-        <Button variant="primary">Cari</Button>
+        <Button variant="primary" onClick={onCari}>
+          Cari
+        </Button>
       </Col>
     </Row>
   );
