e688585b0f6c165d00ec179d04771c3a74dd3466
Sarewes <35771197+sarewes2310@users.noreply.github.com>
2021-10-16 08:55:19 +0700

    Merapikan code React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,51 +1,32 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Login from "./jsx/pages/login";
 import Landing from "./jsx/pages/landing";
 import AppDataAlamatUserView from "./jsx/components/AppDataAlamatUser/AppDataAlamatUserView";
 import register from "./jsx/pages/register";
 import verification from "./jsx/pages/verification";
-import AddProfileImages from "./jsx/example/AddProfileImages";
-import UpdateProfileImages from "./jsx/example/UpdateProfileImages";
-import { Typeahead } from "react-bootstrap-typeahead";
 import DasboardExample from "./jsx/example/DashboardExample";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <div className="auth-wrapper">
-          <div className="auth-inner">
+    <div className="App">
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <BrowserRouter>
             <Switch>
               <Route path="/register" component={register} />
               <Route path="/verification/:token" component={verification} />
               <Route path="/login" component={Login} />
-              {/* <Route
-                path="/users/multi-address"
-                component={AppDataAlamatUserView}
-              />
-              <Route
-                path="/users/example-component/profile-image/add"
-                component={AddProfileImages}
-              />
-              <Route
-                path="/users/example-component/profile-image/update"
-                component={UpdateProfileImages}
-              />
-              <Route
-                path="/users/example-component/typehead"
-                component={Typeahead}
-              /> */}
               <Route path="/" component={DasboardExample} />
               {/* <Route path="/" component={Landing} /> */}
             </Switch>
-          </div>
+          </BrowserRouter>
         </div>
       </div>
-    </Router>
+    </div>
   );
 }
 
