5822bd6fc2f262c13e6f973985ef2c0e45afa561
armerayfr <armerray@gmail.com>
2022-01-20 13:27:07 +0700

    add feature delete + edit qty in cart

--- models/cartModels.js
+++ models/cartModels.js
@@ -9,6 +9,48 @@ const {
 } = require('../utils/response-handler')
 
 module.exports = {
+    getCartMdl: async function (response, searchStatement, getStatement, id, data) {
+
+        try {
+            // column
+            // Inject to sql: check if user in database
+            const searchUser = await db.query(searchStatement, id).catch(err => {
+                throw {
+                    message: 'Gagal menemukan user',
+                    error: err,
+                    status: 500
+                }
+            })
+
+            // const columnCart = ['app_carts_produk.id_user', 'nm_master_produk', 'quantity', 'harga', "URL", 'app_carts_produk.quantity * app_master_produk.harga AS total_produk']
+            const table = ['app_carts_produk INNER JOIN app_master_produk ON app_carts_produk.id_master_barang = app_master_produk.id_master_produk INNER JOIN sys_user ON app_carts_produk.id_user = sys_user.id_user', id]
+            console.log(searchUser);
+            console.log(getStatement);
+            if (searchUser.length) {
+                const carts = await db.query(getStatement, id).catch(err => {
+                    throw {
+                        message: 'Gagal menemukan cart',
+                        error: err,
+                        status: 500
+                    }
+                })
+
+                console.log(carts);
+                responseData(response, 200, carts)
+            } else {
+                throw {
+                    message: "Data tidak ditemukkan",
+                    status: 404,
+                    succes: false
+                }
+            }
+
+        } catch (err) {
+            responError(response, err.status, err)
+        }
+    },
+
+
     insertProductToCartMdl: async function (response, searchStatement, addStatement, id, data) {
         try {
             // Inject to sql: check if product in database
@@ -19,6 +61,8 @@ module.exports = {
                     status: 500
                 }
             })
+
+            console.log(searchData);
 
             // Inject to sql: insert product
             if (searchData.length) {
@@ -41,6 +85,76 @@ module.exports = {
                 }
             }
         } catch (err) {
+            responError(response, err.status, err)
+        }
+    },
+
+    deleteItemInCartMdl: async function (response, searchStatement, deleteStatement, id) {
+        // Check if product exists in cart
+        try {
+            const searchData = await db.query(searchStatement, id).catch(err => {
+                throw {
+                    message: 'gagal menemukan item di cart',
+                    status: 500,
+                    error: err
+                }
+            })
+
+            // Product exists in cart
+            if (searchData.length) {
+                const deleteData = await db.query(deleteStatement, id).catch(err => {
+                    throw {
+                        message: 'gagal menghapus item di cart',
+                        status: 500,
+                        error: err
+                    }
+                })
+
+                // Respon Delete Success
+                responseMessage(response, 200, 'Sukses menghapus item pada cart Anda')
+            } else {
+                throw {
+                    message: 'item tidak ditemukan di cart',
+                    status: 404,
+                }
+            }
+
+        } catch (err) {
+            responError(response, err.status, err)
+        }
+    },
+
+    editQtyItemInCartMdl: async function (response, searchStatement, editStatement, id, data) {
+        try {
+            // find item in cart
+            const searchData = await db.query(searchStatement, id).catch(err => {
+                throw {
+                    message: 'gagal menemukan item di cart',
+                    status: 500,
+                    error: err
+                }
+            })
+
+            // Check item exists
+            if (searchData.length) {
+                const editQtyInData = await db.query(editStatement, [data, id]).catch(err => {
+                    throw {
+                        message: 'gagal mengubah quantity item di cart',
+                        status: 500,
+                        error: err
+                    }
+                })
+                // Edit response exists 200
+                responseMessage(response, 200, 'Sukses mengubah qty item')
+            } else {
+                // Id cart not exists in cart database 404
+                throw {
+                    message: 'item tidak ditemukan di cart',
+                    status: 404,
+                }
+            }
+        } catch (err) {
+            // error response any status code
             responError(response, err.status, err)
         }
     }
--- routers/cartRouters.js
+++ routers/cartRouters.js
@@ -11,5 +11,9 @@ const routers = express.Router()
 
 // Endpoint cart routers
 routers.post("/add-to-cart/:id_master_barang", cartContollers.addProductToCart)
+routers.get("/get-my-cart", cartContollers.getCartUser)
+routers.delete("/delete-item-in-cart/:id", cartContollers.deleteItemInCart)
+routers.patch("/edit-Qtyitem-in-cart/:id", cartContollers.editQtyItemInCart)
+
 
 module.exports = routers
